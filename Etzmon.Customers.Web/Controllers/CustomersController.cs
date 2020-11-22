32c9bac78acbbfd28dd405c3697f33926a3266fa
xernai <montcorona@gmail.com>
2020-11-21 22:04:57 -0600

    CUST-100 CRUD related to Customers - List code

--- Etzmon.Customers.Web/Controllers/CustomersController.cs
+++ Etzmon.Customers.Web/Controllers/CustomersController.cs
@@ -1,15 +1,30 @@
-﻿using Etzmon.Customers.Web.Models;
+﻿using Dapper;
+using Etzmon.Customers.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace Etzmon.Customers.Web.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly IDbConnection _config;
+
+        public CustomersController(IDbConnection config)
+        {
+            _config = config;
+        }
+
         public IActionResult List()
         {
-            Customer customer = new Customer();
-            customer.FirstName = "Javier";
-            return View(customer);
+            List<Customer> data = new List<Customer>();
+            var reader = _config.Query<Customer>("GetAllCustomers",
+                    commandType: CommandType.StoredProcedure);
+
+            data = reader.ToList();
+
+            return View(data);
         }
     }
 }
--- Etzmon.Customers.Web/Startup.cs
+++ Etzmon.Customers.Web/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +15,18 @@ namespace Etzmon.Customers.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("RepositoryDB");
+
+            services.AddTransient<IDbConnection>(db => new SqlConnection(
+            connectionString));
+
             services.AddControllersWithViews();
         }
 
