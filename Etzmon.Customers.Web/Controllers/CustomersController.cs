9248c20310ff2a2ef33a986909f8b53e5bfdf5c3
xernai <montcorona@gmail.com>
2020-11-21 19:39:50 -0600

    Add project files.

---
+++ Etzmon.Customers.Web/Controllers/CustomersController.cs
@@ -0,0 +1,15 @@
+﻿using Etzmon.Customers.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Etzmon.Customers.Web.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult List()
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Javier";
+            return View(customer);
+        }
+    }
+}
